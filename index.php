60b64b0d9129e889362dc213b81f95dbbafd2035
duncan119511 <duncan119511@gmail.com>
2015-04-15 22:38:06 +0800

    Revert "tim"
    This reverts commit df7ae9c8e428fd79eab4ceefc2b3ef526c6fb060.

--- index.php
+++
@@ -1,7 +0,0 @@
-﻿<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<form name="form" method="post" action="connect.php">
-帳號：<input type="text" name="id" /> <br>
-密碼：<input type="password" name="pw" /> <br>
-<input type="submit" name="button" value="登入" />
-<a href="register.php">申請帳號</a>
-</form>
--- logout.php
+++
@@ -1,8 +0,0 @@
-﻿<?php session_start(); ?>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php
-//將session清空
-unset($_SESSION['id']);
-echo '登出中......';
-echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
-?>
